a211e5d725a30598ac1b46e2e5e5b10d41aefaae
Rafael Freire <rafaelsilvafreire2@gmail.com>
2020-10-04 15:52:07 -0300

    Fetching products from react

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import axios from 'axios'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [match.params.id])
 
   return (
     <>
@@ -24,7 +34,7 @@ const ProductScreen = ({ match }) => {
             <ListGroup.Item>
               <Rating
                 value={product.rating}
-                text={`${product.numReviews} avaliações`}
+                text={`de ${product.numReviews} avaliações`}
               />
             </ListGroup.Item>
             <ListGroup.Item>Preço: R${product.price}</ListGroup.Item>
